00b33bc053f9a38f3455735bd654dd4b8258b9c6
Ivan <vanya.schurupov@yandex.ru>
2025-04-29 21:48:48 +0300

    manifest fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Прогноз погоды по городам мира',
   description: 'Проверьте текущую погоду и прогноз погоды в вашем городе',
-  manifest: 'site.webmanifest'
+  manifest: '/site.webmanifest'
 };
 
 export default function RootLayout({
