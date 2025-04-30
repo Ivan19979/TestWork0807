6ad156c1c20dc8d2cc64e2584635bb298fa34c2a
Ivan <vanya.schurupov@yandex.ru>
2025-04-30 18:17:27 +0300

    added new CEO tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,34 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '../styles/globals.scss';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../styles/globals.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Navbar } from '@/components/Navbar/Navbar';
+import { Navbar } from "@/components/Navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Прогноз погоды по городам мира',
-  description: 'Проверьте текущую погоду и прогноз погоды в вашем городе',
-  manifest: '/site.webmanifest'
+  title: "Прогноз погоды по городам мира",
+  description: "Проверьте текущую погоду и прогноз погоды в вашем городе",
+  icons: "/favicon.png",
+  manifest: "/site.webmanifest",
+  openGraph: {
+    title: "Прогноз погоды по городам мира",
+    description: "Проверьте текущую погоду и прогноз погоды в вашем городе",
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+    images: [
+      {
+        url: "/weather.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Прогноз погоды по городам мира",
+    description: "Проверьте текущую погоду и прогноз погоды в вашем городе",
+    images: ["/weather.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -20,18 +39,9 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/favicon.png" type="image/png" />
+        {/* <link rel="icon" href="/favicon.png" type="image/png" /> */}
+        {/* <link rel="apple-touch-icon" href="/favicon.png" type="image/png" /> */}
         <meta name="theme-color" content="#2563eb" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Прогноз погоды по городам мира" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@shchurupoff" />
-        <meta name="twitter:creator" content="@shchurupoff" />
       </head>
       <body className={inter.className}>
         <Navbar />
